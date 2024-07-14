a279ac3ef1d881f94c2d57d6090b1c43f93a2811
Payton <payton016@gmail.com>
2024-07-14 16:35:37 -0400

    Initial Commit

---
+++ Dessert/DessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertApp.swift
+//  Dessert
+//
+//  Created by Payton Mitchell on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
